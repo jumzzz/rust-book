1279bf947a26d9f7b201eda6aa2795c046bed047
jums <Jummel_Sasil@trendmicro.com>
2022-06-26 18:11:00 +0800

    Deleted target/

--- ch4_ownership/ownership/target/debug/deps/ownership-f7c878dc74e2bb31.d
+++
@@ -1,7 +0,0 @@
-/home/jums/hardcore_projects/rust/projects/rust-book/ch4_ownership/ownership/target/debug/deps/ownership-f7c878dc74e2bb31.s: src/main.rs
-
-/home/jums/hardcore_projects/rust/projects/rust-book/ch4_ownership/ownership/target/debug/deps/ownership-f7c878dc74e2bb31: src/main.rs
-
-/home/jums/hardcore_projects/rust/projects/rust-book/ch4_ownership/ownership/target/debug/deps/ownership-f7c878dc74e2bb31.d: src/main.rs
-
-src/main.rs:
